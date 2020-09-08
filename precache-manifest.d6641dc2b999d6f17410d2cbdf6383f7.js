86e62448c0e2b2635749cf2e633b6b15e6af63f6
HIMANSHU2307 <hvs2394@gmail.com>
2020-09-08 23:38:43 +0530

    Updates

---
+++ precache-manifest.d6641dc2b999d6f17410d2cbdf6383f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6e450fdc7c11de42b8d67494ffa72f",
+    "url": "/TailWind-Partners/index.html"
+  },
+  {
+    "revision": "ce95989f6d4e2b53e7cd",
+    "url": "/TailWind-Partners/static/css/main.85517aea.chunk.css"
+  },
+  {
+    "revision": "92d8c5ac2c2ff1af2444",
+    "url": "/TailWind-Partners/static/js/2.8073dfad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TailWind-Partners/static/js/2.8073dfad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce95989f6d4e2b53e7cd",
+    "url": "/TailWind-Partners/static/js/main.7ee2d52e.chunk.js"
+  },
+  {
+    "revision": "0f7ccdbc88e8bc441550",
+    "url": "/TailWind-Partners/static/js/runtime-main.a4640bc4.js"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/TailWind-Partners/static/media/facebook.422e7665.svg"
+  },
+  {
+    "revision": "59b0c8cb832538da82cba3147939d41d",
+    "url": "/TailWind-Partners/static/media/instagram.59b0c8cb.svg"
+  },
+  {
+    "revision": "89dcd6955d20dc3183df9d8ba408312c",
+    "url": "/TailWind-Partners/static/media/linkedin.89dcd695.svg"
+  }
+]);
